97fa87eb33c13bbc2a59cd6e070ce4909248b337
benwhite0820 <dragon7230@gmail.com>
2023-11-25 17:36:02 +0800

    making http requests

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,10 @@
-console.log('hello wrold');
+const { get } = require('https');
+
+get('https://www.google.com', (res) => {
+  res.on('data', (chunk) => {
+    console.log(chunk);
+  });
+  res.on('end', () => {
+    console.log('no more data');
+  });
+});
