6811c36b97d1c9581af4c3c40779effe1f1e05be
benwhite0820 <dragon7230@gmail.com>
2023-11-29 16:47:32 +0800

    http APIs and routing

--- index.js
+++ index.js
@@ -3,16 +3,31 @@ const http = require('http');
 const PORT = 8080;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-  });
+  if (req.url === '/friends') {
+    // res.writeHead(200, {
+    //   'Content-Type': 'application/json',
+    // });
 
-  res.end(
-    JSON.stringify({
-      name: 'ben',
-      nationality: 'taiwan',
-    })
-  );
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+
+    res.end(
+      JSON.stringify({
+        name: 'ben',
+        nationality: 'taiwan',
+      })
+    );
+  } else if (req.url === '/messages') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<h1>Hello World</h1>');
+    res.write('</body>');
+    res.write('</html>');
+  } else {
+    res.statusCode = '404';
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
