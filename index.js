693db1781e3cf8a2add212528b17e0d2dd5895ea
benwhite0820 <dragon7230@gmail.com>
2023-12-20 17:13:01 +0800

    post requests in express

--- index.js
+++ index.js
@@ -4,8 +4,51 @@ const app = express();
 
 const PORT = 8080;
 
+const friends = [
+  {
+    id: 0,
+    name: 'Elon Musk',
+  },
+  {
+    id: 1,
+    name: 'Tim Cook',
+  },
+];
+
+app.use(express.json());
+
+app.post('/friends', (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: 'Missing friend name',
+    });
+  }
+
+  const newFriend = {
+    name: req.body.name,
+    id: friends.lenght,
+  };
+  res.json(newFriend);
+});
+
 app.get('/get', (req, res) => {
   res.send('hello world');
+});
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
+});
+
+app.get('/friends/:friendId', (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+
+  if (friend) {
+    return res.status(200).json(friend);
+  }
+  return res.status(404).json({
+    error: 'Friend does not exist',
+  });
 });
 
 app.listen(PORT, () => {
